3a9295dc0ceb934fda25841280dc5fed797af266
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2020-10-04 23:43:04 +0700

    add content uri into database contract

--- app/src/main/java/com/ihfazh/simplenote/database/DatabaseContract.java
+++ app/src/main/java/com/ihfazh/simplenote/database/DatabaseContract.java
@@ -1,13 +1,21 @@
 package com.ihfazh.simplenote.database;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class DatabaseContract {
     public static final String TABLE_NAME = "notes_note";
+    public static final String AUTHORITY = "com.ihfazh.simplenote";
+    private static final String SCHEME = "content";
 
     public static class NoteColumns implements BaseColumns{
         public static final String TITLE = "title";
         public static final String DESCRIPTION = "description";
         public static final String DATE = "date";
+
+        public static final Uri CONTENT_URI = new Uri.Builder().scheme(SCHEME)
+                .authority(AUTHORITY)
+                .appendPath(TABLE_NAME)
+                .build();
     }
 }
